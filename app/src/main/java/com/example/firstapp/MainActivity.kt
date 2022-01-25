010f10bcb55c47470be1d5de29f116cc65c32e50
migromarj <miguelromeroarjona@gmail.com>
2022-01-25 16:54:26 +0100

    Functionality to add tasks implemented

--- app/src/main/java/com/example/firstapp/MainActivity.kt
+++ app/src/main/java/com/example/firstapp/MainActivity.kt
@@ -2,11 +2,56 @@ package com.example.firstapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var btnAddTask:Button
+    lateinit var etTask:EditText
+    lateinit var rvTasks:RecyclerView
+
+    lateinit var adapter: TaskAdapter
+
+    var tasks = mutableListOf<String>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initUi()
+    }
+
+    private fun initUi() {
+        initView()
+        initListeners()
+        initRecyclerView()
+    }
+
+    private fun initRecyclerView() {
+        rvTasks.layoutManager = LinearLayoutManager(this)
+        adapter = TaskAdapter(tasks)
+        rvTasks.adapter=adapter
+    }
+
+    private fun initListeners() {
+        btnAddTask.setOnClickListener{
+            addTask()
+        }
+    }
+
+    private fun addTask() {
+        val taskToAdd:String=etTask.text.toString()
+        tasks.add(taskToAdd)
+        adapter.notifyDataSetChanged()
+
+    }
+
+    private fun initView() {
+        btnAddTask = findViewById(R.id.btnAddTask)
+        etTask = findViewById(R.id.etTask)
+        rvTasks = findViewById(R.id.rvTasks)
     }
 
 }
---
+++ app/src/main/java/com/example/firstapp/TaskViewHolder.kt
@@ -0,0 +1,15 @@
+package com.example.firstapp
+
+import android.view.View
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class TaskViewHolder (view:View): RecyclerView.ViewHolder(view){
+
+    private val tvTask:TextView = view.findViewById(R.id.tvTask)
+
+    fun render(task:String){
+        tvTask.text=task
+    }
+
+}
